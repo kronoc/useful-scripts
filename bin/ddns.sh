72d958df811ce8fccba8397b7fd137d54fd5a930
conor.keegan <con@conor.net>
2020-11-12 15:38:47 +0000

    updated ddns script to pick up token from a config file

--- bin/ddns.sh
+++ bin/ddns.sh
@@ -1,8 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 # register a subdomain at https://www.duckdns.org/ to get your token
-# set the following in .bashrc
+#add to crontab:
+#30 03 * * * /home/conor/bin/ddns.sh
+# set the following in ~/.ddns
 # export DDNS_SUBDOMAIN="duckdns.test"
 # export DDNS_TOKEN="dummy-token"
-
+source ${HOME}/.ddns
 curl --silent "https://www.duckdns.org/update?domains=$DDNS_SUBDOMAIN&token=$DDNS_TOKEN&ip=" >/dev/null 2>&1
+
