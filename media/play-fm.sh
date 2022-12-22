a3e8eec4040855ab353ae981a3d963fbb49d79e1
conor.keegan <con@conor.net>
2022-12-22 15:53:11 +0000

    fixed domain name

--- media/play-fm.sh
+++ media/play-fm.sh
@@ -1,4 +1,4 @@
 STATION="$@";
-STREAM=$(/usr/bin/curl http://localhost:9981/playlist/channels 2>/dev/null | grep -A1 "$STATION" | grep "stream" | head -n1);
+STREAM=$(/usr/bin/curl http://baird.local:9981/playlist/channels 2>/dev/null | grep -A1 "$STATION" | grep "stream" | head -n1);
 echo $STREAM;
 mplayer -quiet -really-quiet -ao alsa:device=hw=1.0 -vo null -vc null "$STREAM"
